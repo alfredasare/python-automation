4a4de1ab92bcc8796507bdcb8796c8d6908d1c64
alfredasare <alfredasare101@gmail.com>
2023-04-15 23:20:08 +0000

    Handle connection error

--- monitor-website.py
+++ monitor-website.py
@@ -1,8 +1,34 @@
 import requests
+import smtplib
+import os
 
-response = requests.get("http://172-104-226-116.ip.linodeusercontent.com:8080")
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
-if response.status_code == 200:
-    print("Application is running")
-else:
-    print("Application Down. Fix it")
+
+def send_notification(email_msg):
+    print('Sending an email...')
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        message = f"Subject: SITE DOWN\n{email_msg}"
+        smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, message)
+
+
+def monitor_application():
+    try:
+        response = requests.get("http://172-104-226-116.ip.linodeusercontent.com:8080")
+        if response.status_code == 200:
+            print("Application is running")
+        else:
+            print("Application Down. Fix it")
+            msg = f'Application returned {response.status_code}'
+            send_notification(msg)
+    except Exception as ex:
+        print(f'Connection error happened: {ex}')
+        msg = 'Application not accessible at all'
+        send_notification(msg)
+
+
+monitor_application()
